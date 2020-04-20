c257d3301f908d4a5fd5a603199b402b58a1bc6b
Gean Lucas <gean.lfpsa@gmail.com>
2020-04-20 17:04:38 -0300

    Criação da pagina web mostrando os usuarios

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://gean:gean@cluster0-e0thv.mongodb.net/test?retryW
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
