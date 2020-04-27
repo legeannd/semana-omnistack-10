57c5b4625a484021e30faa5dcbc44ed8be907606
Gean Lucas <gean.lfpsa@gmail.com>
2020-04-27 16:10:28 -0300

    Adição da webview no aplicativo

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native'; 
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'; 
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 
-function Main(){
+function Main( {navigation} ){
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -32,13 +32,49 @@ function Main(){
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map} />
+    return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{latitude: -9.6987457, longitude: -36.5769928}}>
+            <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/24641532?s=460&u=aac779f39774da3e5ff1d39298c937b6a97f5567&v=4' }}/>
+            <Callout onPress={() => {
+                navigation.navigate('Profile', { github_username: 'legeannd'});
+            }}>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Gean Lucas</Text>
+                    <Text style={styles.devBio}>Computer Science student - Learning how to say "Hello World" in different ways</Text>
+                    <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+                </View>
+            </Callout>
+        </Marker>
+    </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
     },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    callout: {
+        width: 260
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+    devTechs: {
+        marginTop: 4
+    }
 })
 
 export default Main;
