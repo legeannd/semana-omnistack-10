11863981cf65799ef4ab6ce9aab5dab76d411f97
Gean Lucas <gean.lfpsa@gmail.com>
2020-05-11 16:13:34 -0300

    fix main

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -47,7 +47,6 @@ function Main( {navigation} ){
     }
 
     function handleRegionChanged(region) {
-        console.log(region);
         setCurrentRegion(region);
     }
 
@@ -57,27 +56,35 @@ function Main( {navigation} ){
 
     return (
     <>
-    <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map}>
-        {devs.map(dev => (
-            <Marker 
-                key={dev._id} 
-                coordinate={{latitude: dev.location.coordinates[1], longitude: dev.location.coordinates[0]}}>
-                <Image 
-                    style={styles.avatar} 
-                    source={{ uri: dev.avatar_url }}/>
-                <Callout 
-                    onPress={() => {
-                        navigation.navigate('Profile', { github_username: dev.github_username});
-                }}>
-                <View style={styles.callout}>
-                    <Text style={styles.devName}>{dev.name}</Text>
-                    <Text style={styles.devBio}>{dev.bio}</Text>
-                    <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
-                </View>
-            </Callout>
-        </Marker>
-        ))};
+    	<MapView 
+        onRegionChangeComplete={handleRegionChanged} 
+        initialRegion={currentRegion} 
+        style={styles.map}
+      > 
+        {devs.map(dev =>(
+					<Marker 
+						key={dev._id}
+						coordinate={{
+							latitude: dev.location.coordinates[1], 
+							longitude: dev.location.coordinates[0],
+						}}
+					> 
+					<Image 
+						style={styles.avatar} 
+						source={{ uri: dev.avatar_url }}/> 
+					<Callout onPress={() => { 
+						navigation.navigate('Profile', { github_username: dev.github_username}); 
+					}}>  
+						<View style={styles.callout}> 
+							<Text style={styles.devName}>{dev.name}</Text> 
+							<Text style={styles.devBio}>{dev.bio}</Text> 
+							<Text style={styles.devTechs}>{dev.techs.join(', ')}</Text> 
+						</View> 
+					</Callout> 
+					</Marker> 
+				))}
     </MapView>
+
     <View style={styles.searchForm}>
         <TextInput
             style={styles.searchInput}
